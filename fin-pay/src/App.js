afad032ba630d2f8b3b1f1dee451ed02b800ccd2
dev-sydney <sydney279@icoud.com>
2021-12-11 22:42:52 +0000

    downloaded some dev dependencies and did some file cleanup

--- fin-pay/src/App.js
+++ fin-pay/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import LeftPaneComponent from './components/panes/LeftPaneComponent';
+import RightPaneComponent from './components/panes/RightPaneComponent';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LeftPaneComponent />
+      <RightPaneComponent />
     </div>
   );
 }
