4132d9cc377db15c810af92acc57e415be24617e
dev-sydney <sydney279@icoud.com>
2021-12-13 22:15:55 +0000

    added a carosuel like component which will hold all the cards

---
+++ fin-pay/src/components/overview/cards/CardsCarosuel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import carosuelStyle from './carosuelStyle.scss';
+
+const CardCarosuel = () => {
+  return (
+    <div className="card_carosuel">
+      <h3>Your Cards</h3>
+    </div>
+  );
+};
+export default CardCarosuel;
