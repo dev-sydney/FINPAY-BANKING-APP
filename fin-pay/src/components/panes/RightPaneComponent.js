3ea1a4a1c1b0e98b38db412b2aa3d27482c697d6
dev-sydney <sydney279@icoud.com>
2021-12-13 13:55:35 +0000

    Added the form for inputting the senders name and transfer amount

--- fin-pay/src/components/panes/RightPaneComponent.js
+++ fin-pay/src/components/panes/RightPaneComponent.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import paneStyle from './paneStyle.scss';
 
 import BalanceComponent from '../overview/balance/BalanceComponent';
 import TransactionItem from '../overview/transItem/TransactionItem';
+import CardCarosuel from '../overview/cards/CardsCarosuel';
+import PaymentComponent from '../overview/payment/PaymentComponent';
 
 const RightPaneComponent = () => {
+  const [userPin, setUserPin] = useState('');
+  const [user, setUser] = useState('');
+
+  const onUserChange = e => {
+    setUser(e.target.value);
+  };
+  const onPinChange = e => {
+    setUserPin(e.target.value);
+  };
+
+  /*  useEffect(()=>{
+
+  },[]) */
+
   return (
     <div className="right_pane">
       <header className="header_panel">
         <h3>Overiew</h3>
         <p>Hi Sydney, welcome back</p>
+        <form>
+          <input
+            className="userNameInput"
+            placeholder="user"
+            value={user}
+            onChange={onUserChange}
+            type="text"
+          />
+          <input
+            className="userPinInput"
+            placeholder="PIN"
+            value={userPin}
+            onChange={onPinChange}
+            type="text"
+            maxLength="4"
+          />
+        </form>
       </header>
-      <BalanceComponent />
-      <div className="transactions_container">
-        <TransactionItem />
+      <div className="dash_board">
+        <BalanceComponent />
+        <PaymentComponent />
+        <div className="transactions_container">
+          <TransactionItem />
+        </div>
+        <CardCarosuel />
       </div>
     </div>
   );
