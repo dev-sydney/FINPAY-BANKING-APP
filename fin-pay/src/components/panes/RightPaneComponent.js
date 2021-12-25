720949d55cefdbeffff77a5fe357074ac467aa91
dev-sydney <sydney279@icoud.com>
2021-12-25 13:44:41 +0000

    Implemented the display of transaction Summaries

--- fin-pay/src/components/panes/RightPaneComponent.js
+++ fin-pay/src/components/panes/RightPaneComponent.js
@@ -1,14 +1,32 @@
+/**CORE-IMPORTS */
 import React, { useState, useEffect } from 'react';
-import paneStyle from './paneStyle.scss';
+import { connect } from 'react-redux';
 
+import paneStyle from './paneStyle.scss';
+/**COMPONENT-IMPORTS */
 import BalanceComponent from '../overview/balance/BalanceComponent';
 import TransactionItem from '../overview/transItem/TransactionItem';
 import CardCarosuel from '../overview/cards/CardsCarosuel';
 import PaymentComponent from '../overview/payment/PaymentComponent';
+import LoginComponent from '../login/LoginComponent';
 
-const RightPaneComponent = () => {
+/**ACTION-IMPORTS */
+import { calcSummaries } from '../../actions/userActions';
+
+const RightPaneComponent = ({
+  app: { currentUser, incomeSumm, transferSumm },
+  calcSummaries,
+}) => {
+  useEffect(() => {
+    if (currentUser) {
+      calcSummaries(currentUser);
+    }
+    //eslint-disable-next-line
+  }, [currentUser, incomeSumm, transferSumm]);
+
   const [userPin, setUserPin] = useState('');
   const [user, setUser] = useState('');
+  const [showApp, setShowApp] = useState(false);
 
   const onUserChange = e => {
     setUser(e.target.value);
@@ -17,42 +35,58 @@ const RightPaneComponent = () => {
     setUserPin(e.target.value);
   };
 
-  /*  useEffect(()=>{
-
-  },[]) */
-
   return (
     <div className="right_pane">
-      <header className="header_panel">
-        <h3>Overiew</h3>
-        <p>Hi Sydney, welcome back</p>
-        <form>
-          <input
-            className="userNameInput"
-            placeholder="user"
-            value={user}
-            onChange={onUserChange}
-            type="text"
-          />
-          <input
-            className="userPinInput"
-            placeholder="PIN"
-            value={userPin}
-            onChange={onPinChange}
-            type="text"
-            maxLength="4"
-          />
-        </form>
-      </header>
-      <div className="dash_board">
-        <BalanceComponent />
-        <PaymentComponent />
-        <div className="transactions_container">
-          <TransactionItem />
+      {!showApp ? (
+        <LoginComponent setShowApp={setShowApp} />
+      ) : (
+        <div className="overview_board">
+          <header className="header_panel">
+            <h3>Overiew</h3>
+            <p>
+              Hi {currentUser && currentUser.ownerFullName.split(' ')[0]},
+              welcome back
+            </p>
+            <form>
+              <input
+                className="userNameInput"
+                placeholder="user"
+                value={user}
+                onChange={onUserChange}
+                type="text"
+              />
+              <input
+                className="userPinInput"
+                placeholder="PIN"
+                value={userPin}
+                onChange={onPinChange}
+                type="text"
+                maxLength="4"
+              />
+            </form>
+          </header>
+          <div className="dash_board">
+            <BalanceComponent />
+            <PaymentComponent />
+            <div className="transactions_container">
+              <div className="summaries">
+                <span style={{ color: 'green' }}>
+                  {incomeSumm && incomeSumm}
+                </span>
+                <span style={{ color: 'red' }}>
+                  {transferSumm && transferSumm}
+                </span>
+              </div>
+              <TransactionItem />
+            </div>
+            <CardCarosuel />
+          </div>
         </div>
-        <CardCarosuel />
-      </div>
+      )}
     </div>
   );
 };
-export default RightPaneComponent;
+const mapStateToProps = state => ({
+  app: state.app,
+});
+export default connect(mapStateToProps, { calcSummaries })(RightPaneComponent);
