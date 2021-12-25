8879a5252ab61486c055ac5350d3dbb1b4acabc8
dev-sydney <sydney279@icoud.com>
2021-12-25 13:50:05 +0000

    some minor changes

--- fin-pay/src/reducers/index.js
+++ fin-pay/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
-import userReducer from './userReducer';
+import appReducer from './appReducer';
 
 export default combineReducers({
-  user: userReducer,
+  app: appReducer,
 });
