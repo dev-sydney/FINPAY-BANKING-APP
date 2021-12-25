edc36b40344d728d550f76a372d480668762491c
dev-sydney <sydney279@icoud.com>
2021-12-25 13:55:01 +0000

    Created the global reducer

---
+++ fin-pay/src/components/login/LoginComponent.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+
+import loginStyle from './loginStyle.scss';
+import { loginValidilityCheck } from '../../actions/userActions';
+
+const LoginComponent = ({
+  setShowApp,
+  loginValidilityCheck,
+  app: { accounts },
+}) => {
+  const [user, setUser] = useState('');
+  const [userPin, setUserPin] = useState('');
+
+  const onUserChange = e => {
+    setUser(e.target.value);
+  };
+  const onUserPinChange = e => {
+    setUserPin(+e.target.value);
+  };
+  const onLoginSubmit = e => {
+    e.preventDefault();
+
+    const isLoginvalid = loginValidilityCheck(user, userPin, accounts);
+
+    setShowApp(isLoginvalid);
+  };
+  return (
+    <Fragment>
+      <form className="login_form" onSubmit={onLoginSubmit}>
+        <input
+          type="text"
+          placeholder="user"
+          onChange={onUserChange}
+          value={user}
+        />
+        <input
+          type="text"
+          placeholder="PIN"
+          maxLength={4}
+          onChange={onUserPinChange}
+          value={userPin}
+        />
+        <button type="submit">login</button>
+      </form>
+    </Fragment>
+  );
+};
+
+const mapStateToProps = state => ({
+  app: state.app,
+});
+
+export default connect(mapStateToProps, { loginValidilityCheck })(
+  LoginComponent
+);
