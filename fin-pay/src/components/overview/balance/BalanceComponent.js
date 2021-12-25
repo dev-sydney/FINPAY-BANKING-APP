251847c4ee32fa0dcb2329d2cb11967357dfe776
dev-sydney <sydney279@icoud.com>
2021-12-25 13:30:30 +0000

    Implemented the logic for calculating and displaying the baalnce

--- fin-pay/src/components/overview/balance/BalanceComponent.js
+++ fin-pay/src/components/overview/balance/BalanceComponent.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import balStyle from './balStyle.scss';
+import { connect } from 'react-redux';
 
-const BalanceComponent = () => {
+const BalanceComponent = ({ app: { currentUser } }) => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    if (currentUser)
+      setBalance(
+        currentUser.movements.reduce((prev, curr) => {
+          return prev + curr;
+        }, 0)
+      );
+    //eslint-disable-next-line
+  }, [currentUser]);
+
   return (
     <div className="bal_card">
       <p>Your Balance</p>
-      <h3>GHS 2000,000,00.00</h3>
+      <h3>{currentUser ? balance : 800}</h3>
       <p>Saving A/C: xxxxxxxxx6789</p>
     </div>
   );
 };
 
-export default BalanceComponent;
+const mapStateToProps = state => ({
+  app: state.app,
+});
+
+export default connect(mapStateToProps, null)(BalanceComponent);
