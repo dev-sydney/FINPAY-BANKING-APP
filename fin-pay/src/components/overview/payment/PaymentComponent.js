9ae86ddbac6d08497d7c5d8e6e6c60dc16e96e73
dev-sydney <sydney279@icoud.com>
2021-12-28 21:22:33 +0000

    Integrated the payment component  UI with the transactionValidlity Function

--- fin-pay/src/components/overview/payment/PaymentComponent.js
+++ fin-pay/src/components/overview/payment/PaymentComponent.js
@@ -1,32 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import paymentStyle from './paymentStyle.scss';
 
-const PaymentComponent = () => {
-  const [senderName, setSenderName] = useState('');
+import { connect } from 'react-redux';
+
+import { TransactionValidility } from '../../../actions/userActions';
+
+const PaymentComponent = ({
+  app: { movements, accounts, currentUser },
+  TransactionValidility,
+}) => {
+  const [avBalance, setAvBalance] = useState(0);
+
+  useEffect(() => {
+    if (movements.length > 0)
+      setAvBalance(
+        movements.reduce((prev, curr) => {
+          return prev + curr;
+        }, 0)
+      );
+    //eslint-disable-next-line
+  }, [movements]);
+
+  const [recepientName, setRecepientName] = useState('');
   const [transferAmount, setTransferAmount] = useState('');
   /**
-   * Responsible for handling the change Event in the senderName input
+   * Responsible for handling the change Event in the recepientName input
    * @param {*} e
    */
   const onUserChange = e => {
-    setSenderName(e.target.value);
+    setRecepientName(e.target.value);
   };
   /**
    * Responsible for handling the change Event in the TransferAmount input
    * @param {*} e
    */
   const onAmountChange = e => {
-    setTransferAmount(e.target.value);
+    setTransferAmount(+e.target.value);
   };
+  const onFormSubmit = e => {
+    e.preventDefault();
+    if (recepientName !== currentUser.owner) {
+      TransactionValidility(
+        recepientName,
+        accounts,
+        transferAmount,
+        avBalance,
+        currentUser
+      );
+    }
+  };
+
   return (
     <div className="payment_card">
       <p>Quick transfer</p>
-      <form>
+      <form onSubmit={onFormSubmit}>
         <input
           type="text"
           placeholder="user"
           onChange={onUserChange}
-          value={senderName}
+          value={recepientName}
         />
         <input
           type="text"
@@ -34,10 +66,16 @@ const PaymentComponent = () => {
           onChange={onAmountChange}
           value={transferAmount}
         />
+        <button type="submit">Send</button>
       </form>
-      <button type="submit">Send</button>
     </div>
   );
 };
 
-export default PaymentComponent;
+const mapStateToProps = state => ({
+  app: state.app,
+});
+
+export default connect(mapStateToProps, { TransactionValidility })(
+  PaymentComponent
+);
