67ac1dff06126ad17888e8aa1936e05044af73e7
dev-sydney <sydney279@icoud.com>
2022-01-15 16:39:47 +0000

    Added The date of transaction to a single transaction card component

--- fin-pay/src/components/overview/transItem/TransactionItem.js
+++ fin-pay/src/components/overview/transItem/TransactionItem.js
@@ -5,16 +5,23 @@ import transItemStyle from './transItemStyle.scss';
  * Renders a Transaction that has been made
  * @returns {Element} An element created using JSX is returned
  */
-const TransactionItem = () => {
+const TransactionItem = ({ transActions }) => {
+  console.log(transActions);
   return (
     <div className="transaction">
-      <span className="transaction_info">
-        <p>
-          Transfer to <strong>Sydney</strong>
-        </p>
-        <p>Reference</p>
-      </span>
-      <span className="transaction_amount">GHS 2000</span>
+      <strong>{new Date(transActions[0].mvtDate).toLocaleDateString()}</strong>
+      {transActions.map(transAct => (
+        <div className="Info_Amount" key={transAct.id}>
+          <span className="transaction_info">
+            <p>
+              {transAct.mvtAmt < 0 ? 'Transfer To' : 'Recieved from'}
+              <strong>{transAct.mvtWith}</strong>
+            </p>
+            <p>Reference</p>
+          </span>
+          <span className="transaction_amount">{transAct.mvtAmt}</span>
+        </div>
+      ))}
     </div>
   );
 };
