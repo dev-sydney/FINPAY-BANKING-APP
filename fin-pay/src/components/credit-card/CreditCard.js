ec817749727b37a6ce7f59e6799a49af6ca44192
dev-sydney <sydney279@icoud.com>
2022-02-07 15:34:28 +0000

    Some minor bug fixes:mostly styling errors

--- fin-pay/src/components/credit-card/CreditCard.js
+++ fin-pay/src/components/credit-card/CreditCard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ccStyle from './ccStyle.scss';
 
-const CreditCard = () => {
+const CreditCard = ({ cardObj }) => {
   return (
     <div className="card_component">
       <span>logo</span>
       <p className="card_num" style={{ margin: '0%' }}>
-        card number
+        {cardObj && cardObj.CardNumber}
       </p>
       <div className="holder_expiry">
         <span className="holder_details">
           <p style={{ margin: '0%' }}>CARD HOLDER</p>
-          <p style={{ margin: '0%' }}>My Name</p>
+          <p style={{ margin: '0%' }}> {cardObj && cardObj.cardHolder}</p>
         </span>
         <span className="expiry_details">
           <p style={{ margin: '0%' }}>EXPIRES</p>
-          <p style={{ margin: '0%' }}>My Name</p>
+          <p style={{ margin: '0%' }}>{cardObj && cardObj.expiry}</p>
         </span>
       </div>
     </div>
