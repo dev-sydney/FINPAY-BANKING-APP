3669757fe8a5314f6e550a01ba81e4b35ed36730
dev-sydney <sydney279@icoud.com>
2022-02-21 16:06:13 +0000

    Refactored the components code to recieve an Object as a prop-used to be an Array and changed the way it renders information

--- fin-pay/src/components/overview/transItem/TransactionItem.js
+++ fin-pay/src/components/overview/transItem/TransactionItem.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import transItemStyle from './transItemStyle.scss';
 
-/**
- * Renders a Transaction that has been made
- * @returns {Element} An element created using JSX is returned
- */
-const TransactionItem = ({ transActions }) => {
+const TransactionItem = ({ transAction }) => {
+  useEffect(() => {
+    if (transAction) {
+      console.log(transAction);
+    }
+    //eslint-disable-next-line
+  }, [transAction]);
   return (
     <div className="transaction">
-      <strong>{new Date(transActions[0].mvtDate).toLocaleDateString()}</strong>
-      {transActions.map(transAct => (
-        <div className="Info_Amount" key={transAct.id}>
+      <strong>{new Date(transAction.mvtDate).toLocaleDateString()}</strong>
+      {
+        <div className="Info_Amount">
           <span className="transaction_info">
             <p>
-              {transAct.mvtAmt < 0 ? 'Transfer To' : 'Recieved from'}
-              <strong>{transAct.mvtWith}</strong>
+              {transAction.mvtAmt < 0 ? 'Transfer To' : 'Recieved from'}
+              <strong>{transAction.mvtWith}</strong>
             </p>
             <p>Reference</p>
           </span>
-          <span className="transaction_amount">{transAct.mvtAmt}</span>
+          <span className="transaction_amount">{transAction.mvtAmt}</span>
         </div>
-      ))}
+      }
     </div>
   );
 };
